d7f384fa50afe53a86e4ab931245d31c4e2b0cb4
sergeyman <sergey_man@ukr.net>
2020-06-13 23:02:15 +0300

    Adding mocha, chai page test 1 (title test)

--- meadowlark.js
+++ meadowlark.js
@@ -7,13 +7,18 @@ var fortune = require('./lib/fortune.js');
 // Установка механизма представления handlebars
 var handlebars = require('express-handlebars')
     .create({ defaultLayout:'main' });                  //указываем макет по умолчанию
-app.engine('hbs', handlebars.engine);
-app.set('view engine', 'hbs');
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 app.set('views', './views');
 
 app.use(express.static(__dirname + '/public'));
 
-
+// set 'showTests' context property if the querystring contains test=1
+app.use(function(req, res, next){
+	res.locals.showTests = app.get('env') !== 'production' && 
+		req.query.test === '1';
+	next();
+});
 
 app.get('/', function(req, res){
     //res.type('text/plain');
